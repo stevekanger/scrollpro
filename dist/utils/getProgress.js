4c60a0c2e2d57df6175bc88ff0e731dffcef890c
SteveKanger <hi.stevekanger@gmail.com>
2023-05-07 22:16:57 -0500

    Cleaned up getProgress

--- dist/utils/getProgress.js
+++ dist/utils/getProgress.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 function getProgress(start, distance, scrollY) {
     var progress = Math.min(1, Math.max(0, (scrollY - start) / distance));
-    return isNaN(progress) ? 0 : progress;
+    return progress ? progress : 0;
 }
 exports.default = getProgress;
--- src/utils/getProgress.ts
+++ src/utils/getProgress.ts
@@ -1,6 +1,6 @@
 function getProgress(start: number, distance: number, scrollY: number) {
   const progress = Math.min(1, Math.max(0, (scrollY - start) / distance))
-  return isNaN(progress) ? 0 : progress
+  return progress ? progress : 0
 }
 
 export default getProgress
