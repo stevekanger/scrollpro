6a4de17642ff10605a97b6d5e2aad6d4709d6d3f
SteveKanger <hi.stevekanger@gmail.com>
2023-05-07 22:00:22 -0500

    Fixed getProgress NAN issue

--- dist/utils/getProgress.js
+++ dist/utils/getProgress.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function getProgress(start, distance, scrollY) {
-    return Math.min(1, Math.max(0, (scrollY - start) / distance));
+    var progress = Math.min(1, Math.max(0, (scrollY - start) / distance));
+    return isNaN(progress) ? 0 : progress;
 }
 exports.default = getProgress;
--- src/utils/getProgress.ts
+++ src/utils/getProgress.ts
@@ -1,5 +1,6 @@
 function getProgress(start: number, distance: number, scrollY: number) {
-  return Math.min(1, Math.max(0, (scrollY - start) / distance))
+  const progress = Math.min(1, Math.max(0, (scrollY - start) / distance))
+  return isNaN(progress) ? 0 : progress
 }
 
 export default getProgress
